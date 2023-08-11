66be6777ff7410331df5fab4202987939d24b9fd
Данил Царев <Drsif@yandex.ru>
2023-08-11 23:11:23 +0300

    New Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['drsif.servegame.com']
+ALLOWED_HOSTS = ['158.160.67.18', '127.0.0.1', 'localhost', 'drsif.servegame.com']
 
 
 # Application definition
